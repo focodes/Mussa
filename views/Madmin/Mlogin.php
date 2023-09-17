1ad95afaef81d91b41d1a6f9789f7e689081d7c5
charlis01emanuel <144944035+charlis01emanuel@users.noreply.github.com>
2023-09-16 20:24:02 -0400

    icon

--- views/Madmin/Mlogin.php
+++ views/Madmin/Mlogin.php
@@ -8,6 +8,7 @@
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
       <!-- Estilos personalizados -->
       <link rel="stylesheet" href="admincss/cssadmin.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
    </head>
    <body>
